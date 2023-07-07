23ee1a0de824770aa49b5223111b461fcb5352cd
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2023-07-07 13:46:23 +0200

    add transaction and error callnack to query helper function

--- server/utils/databaseQuery.js
+++ server/utils/databaseQuery.js
@@ -1,13 +1,19 @@
-import { pool } from "../config/database.js";
+import pool from "../config/database.js";
 
-export async function executeQuery(query, values = []) {
+export async function executeQuery(query, values = [], cb) {
   const client = await pool.connect();
 
   try {
+    await client.query("BEGIN");
+
     const result = await client.query(query, values);
+
+    await client.query("COMMIT");
+
     return result.rows;
   } catch (err) {
-    console.log(err);
+    await client.query("ROLLBACK");
+    return cb(err);
   } finally {
     client.release(true);
   }
