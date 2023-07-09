4d0411d54f820a95fed84b359e553271438f5878
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2023-07-09 16:48:05 +0200

    add return value in User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 import { executeQuery } from "../utils/databaseQuery.js";
-import { errorQueryDataBase } from "../utils/error.js";
+import { errorQueryDataBase } from "../utils/error/databaseError.js";
 
 const User = {
   createLocal: async function createUserLocal(userInfo) {
@@ -16,7 +16,7 @@ const User = {
     try {
       const userQuery = `INSERT INTO users (username, role, bot, affinity_name, homeworld_name, email)
                          VALUES($1, $2, $3, $4, $5, $6)
-                         RETURNING users.id
+                         RETURNING users.id, users.username, users.homeworld_name as homworld, affinity_name as affinity
     `;
       const userValues = [
         username,
@@ -26,23 +26,26 @@ const User = {
         homeworld_name,
         email,
       ];
-      const response = await executeQuery(
+      const user = await executeQuery(
         userQuery,
         userValues,
         errorQueryDataBase
       );
-      const userId = response[0].id;
+      const userId = user[0].id;
       const credentialsQuery = `INSERT INTO users_credentials (user_id, password)
-                                VALUES ($1, $2)`;
+                                VALUES ($1, $2)
+                                RETURNING *
+                                `;
       const credentialsValues = [userId, password];
       await executeQuery(
         credentialsQuery,
         credentialsValues,
         errorQueryDataBase
       );
+      return user;
     } catch (err) {
-      console.log(err);
-      return false;
+      console.log(err.message);
+      return { success: false, errorMessage: err.message };
     }
   },
   createOauth: async function createUserOauth(userInfo) {
@@ -61,7 +64,7 @@ const User = {
     try {
       const userQuery = `INSERT INTO users (username, role, bot, affinity_name, homeworld_name, email, oauth_provider, oauth_id, oauth_access_token)
                          VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
-                         RETURNING users.id 
+                         RETURNING users.id, users.username, users.homeworld_name as homworld, affinity_name as affinity
     `;
       const userValues = [
         username,
@@ -74,15 +77,17 @@ const User = {
         oauth_id,
         oauth_access_token,
       ];
-      const response = await executeQuery(
+      const user = await executeQuery(
         userQuery,
         userValues,
         errorQueryDataBase
       );
-      return response;
+      return user;
     } catch (err) {
       console.log(err);
-      return false;
+      return { success: false, errorMessage: err.message };
     }
   },
 };
+
+export default User;
