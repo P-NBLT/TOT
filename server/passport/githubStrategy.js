dcfc8f19e40be2598166c1c9334a4d5bf71bb071
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2023-07-13 12:15:24 +0200

    cleanup

--- server/passport/githubStrategy.js
+++ server/passport/githubStrategy.js
@@ -5,7 +5,7 @@ function initialize(passport, User) {
   const clientID = config.GITHUB_CLIENT_ID;
   const clientSecret = config.GITHUB_CLIENT_SECRET;
   const callbackURL = config.GITHUB_CALLBACK_URI;
-  console.log("whats up");
+
   passport.use(
     new githubStrategy(
       {
@@ -14,13 +14,6 @@ function initialize(passport, User) {
         callbackURL,
       },
       async function (accessToken, refreshToken, profile, done) {
-        console.log(
-          "Trigger github strategy",
-          profile,
-          profile._json,
-          accessToken,
-          refreshToken
-        );
         const newUser = await User.createOrFindOauth({
           id: profile._json.id,
           email: profile._json.email,
