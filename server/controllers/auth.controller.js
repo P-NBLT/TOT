790eb6fbb59d19546673389ab45ef68ec9b2d0f1
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2023-07-13 12:15:07 +0200

    add error handling for local strategy + refactor auth controler

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,42 +1,65 @@
+import passport from "passport";
+import User from "../models/User.js";
 import * as authServices from "../services/auth.service.js";
+import { v4 as uuidv4 } from "uuid";
 
-export async function createUser(req, res) {
-  const {
-    username,
-    password,
-    affinity_name,
-    email,
-    oauth_provider,
-    oauth_id,
-    oauth_access_token,
-  } = req.body;
+export async function createUser(req, res, next) {
+  const { password, email } = req.body;
+  const token = uuidv4();
 
   //if we are going to register the user
   //from a password authentification then
-  if (password) {
-    const user = await authServices.createUserLocal({
-      username,
-      password,
-      affinity_name,
-      email,
-    });
-    console.log(user);
-    if (user.success === false) {
-      res.status(409).json({ message: user.errorMessage });
-    } else res.status(201).json(user);
+
+  const user = await User.createLocal({ email, password, token });
+
+  if (user.success === false) {
+    res.status(409).json({ message: user.errorMessage });
+  } else {
+    await authServices.sendEmailVerification(email, token);
+    return res.status(201).json({ message: "success" });
   }
-  // otherwise user registered with OAuth
-  else {
-    const user = await authServices.createUserOAuth({
-      username,
-      affinity_name,
-      email,
-      oauth_provider,
-      oauth_id,
-      oauth_access_token,
-    });
-    if (user.success === false) {
-      res.status(409).json({ message: user.errorMessage });
-    } else res.status(201).json(user);
+}
+
+export async function setUserAsVerified(req, res) {
+  const { token } = req.query;
+  try {
+    const verifiedUser = await User.setLocalUserToVerified(token);
+    if (verifiedUser) {
+      req.login(verifiedUser, (err, done) => {
+        if (err) {
+          return done(err);
+        }
+        return res.redirect("/profile/temp");
+      });
+      // res.status(201).json({user: req.user})
+    }
+  } catch (e) {
+    res.status(501).json({ message: e });
   }
+}
+
+// login is handled with passport. see ./server/utils/passport
+// below is just how we are handling the possbible errors (info) being an error manually thrown
+// req.login is here to serialize the user and store the session data.
+export async function loginUser(req, res) {
+  passport.authenticate("local", (err, user, info) => {
+    //execution error
+    if (err) {
+      return res.status(501).json(err);
+    }
+    //error from the user side
+    if (info) {
+      if (info.type === "email/password") return res.status(401).json(info);
+      if (info.type === "unverified") return res.status(401).json(info);
+    }
+
+    req.login(user, (err) => {
+      if (err) {
+        return res.status(501).json({
+          message: `Something went wrong while login you in: ${err.message}`,
+        });
+      }
+      return res.status(200).json({ user: req.user, session: req.session });
+    });
+  })(req, res);
 }
--- server/passport/localStrategy.js
+++ server/passport/localStrategy.js
@@ -8,11 +8,27 @@ function initialize(passport, User) {
       { usernameField: "email", passwordField: "password" },
       async function verify(email, password, done) {
         const user = await User.findUserByEmail(email);
+
         if (user.success === false) return done(user.errorMessage);
-        if (!user) done(null, false);
+        if (!user) {
+          return done(null, false, {
+            type: "email/pasword",
+            message: "Invalid email or password",
+          });
+        }
+        if (!user.is_verified) {
+          return done(null, false, {
+            type: "unverified",
+            message: "user email not verified. please verify yout email.",
+          });
+        }
 
         const match = await bcrypt.compare(password, user.password);
-        if (!match) done(null, false);
+        if (!match)
+          return done(null, false, {
+            type: "email/pasword",
+            message: "Invalid email or password",
+          });
 
         const profile = await Profile.findProfileByUserID(user.id);
 
