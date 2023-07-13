526066704f3175abf3404af573a3edabf2d40f54
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2023-07-13 12:19:30 +0200

    auth route and service

--- server/services/auth.service.js
+++ server/services/auth.service.js
@@ -1,4 +1,9 @@
 import User from "../models/User.js";
+import Email from "../models/Email.js";
+
+export async function sendEmailVerification(email, token) {
+  await Email.sendVerificationEmail(email, token);
+}
 
 export async function createUserLocal(userInfo) {
   const user = await User.createLocal(userInfo);
