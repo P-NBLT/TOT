eba2296dc1ffb7c9415543530cddc213514cbe58
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2023-07-18 15:21:08 +0200

    profile routes

--- server/controllers/profile.controller.js
+++ server/controllers/profile.controller.js
@@ -15,8 +15,46 @@ export async function createUserProfile(req, res) {
     user.username = username;
     return res.status(201).json({ user: req.user });
   } catch (err) {
+    console.log(err);
     res
       .status(501)
       .json({ message: `Something went wrong while creating the profile` });
+  }
+}
+
+export async function getUserProfile(req, res) {
+  try {
+    const { userId } = req.params;
+    const userProfile = await Profile.findProfileByUserID(userId, [
+      "username, bot",
+    ]);
+    if (userProfile) return res.status(200).json(userProfile);
+    else
+      return res
+        .status(404)
+        .json({ message: "This profile does't seem to exist" });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(501)
+      .json({ message: `Something went wrong while fetching the profile` });
+  }
+}
+
+export async function getUsersProfile(req, res) {
+  try {
+    let options = null;
+    let keys = Object.keys(req.query);
+    if (keys.length > 0) options = keys;
+
+    const usersProfile = await Profile.getAllProfiles(options);
+
+    if (usersProfile) return res.status(200).json(usersProfile);
+    else return res.status(404).json({ message: "Something went wrong" });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(501)
+      .json({ message: `Something went wrong while fetching the profiles` });
   }
 }
