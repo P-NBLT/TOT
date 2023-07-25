c4c589f76004989fe14b814e9b30bf6de851ad1c
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2023-07-25 12:45:44 +0200

    mock data for testing

---
+++ server/test/util/mockData.js
@@ -0,0 +1,17 @@
+const mockData = {
+  auth: {
+    email: "test1@test.com",
+    password: "passwordForTesting",
+    path: "/auth",
+  },
+  profile: {
+    email: "test2@test.com",
+    password: "passwordForTesting",
+    username: "Flegus Mok",
+    affinity: "Empire",
+    side: "Dark Side",
+    path: "/profile",
+  },
+};
+
+export default mockData;
