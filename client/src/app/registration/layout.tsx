8826011c86b659e78dfc5995a085ec7e36fcdebf
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2023-09-01 12:55:27 +0200

    layout registration page

---
+++ client/src/app/registration/layout.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import backgroundImage from "../../assets/images/jabba palace 2.jpeg";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html>
+      <head>
+        <title>Registration</title>
+      </head>
+      <body>
+        <div
+          style={{
+            backgroundImage: `url("${backgroundImage.src}")`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            height: "100vh",
+            width: "100%",
+          }}
+        >
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
