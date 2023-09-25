da177ec908005b8f7af3ed2989d71d4b6aa8542c
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2023-09-25 15:48:06 +0200

    BE: refactor passport module + export session and passport Middleware for socket handshake

--- server/server.js
+++ server/server.js
@@ -2,13 +2,18 @@ import express from "express";
 import cors from "cors";
 import { createServer } from "http";
 import appRouter from "./routes/index.js";
-import { initializePassport, serializePassport } from "./passport/passport.js";
+import {
+  initializePassport,
+  serializePassport,
+  sessionMiddleware,
+  passportMiddleware,
+} from "./passport/passport.js";
 import { createServer as createIoServer } from "./io.js";
 
 export function createHttpServer() {
   const app = express();
   const httpServer = createServer(app);
-  createIoServer(httpServer);
+  createIoServer(httpServer, sessionMiddleware, passportMiddleware);
   app.use(
     cors({
       credentials: true,
