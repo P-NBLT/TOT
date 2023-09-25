404160f1d6284f62e3996f53473fc053e893ff63
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2023-09-25 15:50:52 +0200

    BE: io middleware

--- server/io.js
+++ server/io.js
@@ -1,15 +1,27 @@
 import { Server } from "socket.io";
 
-export function createServer(httpServer) {
+export function createServer(
+  httpServer,
+  sessionMiddleware,
+  passportMiddleware
+) {
   const io = new Server(httpServer, {
-    cors: { origin: "*" },
+    cors: { origin: "http://localhost:3000", credentials: true },
   });
 
-  io.on("connection", async (socket) => {
-    console.log("SOCKET Working", socket.id);
+  io.use((socket, next) => {
+    sessionMiddleware(socket.request, socket.request.res || {}, next);
+  });
 
-    io.on("disconenct", () => {
-      console.log("socket disconnected");
-    });
+  io.use((socket, next) => {
+    passportMiddleware.initialize(
+      socket.request,
+      socket.request.res || {},
+      next
+    );
+  });
+
+  io.use((socket, next) => {
+    passportMiddleware.session(socket.request, socket.request.res || {}, next);
   });
 }
