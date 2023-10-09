00faefb16499a9e0f5cf3c7f8f15cfb398f3ac47
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2023-10-09 21:14:32 +0200

    FE: Date formating

---
+++ client/src/app/utils/date.ts
@@ -0,0 +1,19 @@
+import {
+  format,
+  parseISO,
+  isToday,
+  isYesterday,
+  subDays,
+  isBefore,
+} from "date-fns";
+
+export function formatTime(time: string) {
+  const dateObject = parseISO(time);
+  if (isToday(dateObject)) {
+    return "Today";
+  } else if (isYesterday(dateObject)) {
+    return "Yesterday";
+  } else if (isBefore(dateObject, subDays(new Date(), 2))) {
+    return format(parseISO(time), "dd/MM/yyyy");
+  }
+}
