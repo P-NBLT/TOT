d26d2750338acf403bf40bffbc8dae660f860bf5
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2023-10-09 21:18:06 +0200

    FE: create room + show recent chat in messaging bubble api

--- client/src/app/(portal)/feed/view/messagingBoxCard.tsx
+++ client/src/app/(portal)/feed/view/messagingBoxCard.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import messagingBoxStyle from "./css/messagingBoxCard.module.css";
-import { userMessages } from "./mockup";
 import InboxBubble from "@/app/components/messages/inboxBubble";
 import ConversationBubble from "@/app/components/messages/conversationBubble";
 import { useWindowSize } from "@/app/hooks/useWindowSize";
+import { apiClient } from "@/app/utils/apiClient";
+import { useUser } from "@/app/controllers/userProvider";
 
 type Conversation = {
   username: string;
@@ -12,11 +13,24 @@ type Conversation = {
   open: boolean;
 };
 
+type InboxFeedMessage = {
+  contactName: string;
+  profilePic: string;
+  timestamp: string;
+  content: string;
+  roomId: string;
+};
+
 const MessagingBoxCard: React.FC = () => {
+  const { user } = useUser();
+  const [inboxFeedMessages, setInboxFeedMessages] = useState<
+    InboxFeedMessage[] | []
+  >([]);
   const [conversationBubbles, setConversationBubbles] = useState<
     Conversation[]
   >([]);
   const { width } = useWindowSize();
+  const [offset, setOffset] = useState<number>(0); // logic for incrementing the offset will be added later.
 
   function expandConversation(roomId: string) {
     setConversationBubbles((prevConversation) =>
@@ -29,7 +43,21 @@ const MessagingBoxCard: React.FC = () => {
     return;
   }
 
-  function addConversationToQueue(message: any) {
+  async function addConversationToQueue(message: any) {
+    if (!message.roomId) {
+      var newRoom = await createRoom(user!.id, message.contactId);
+      message.roomId = newRoom.roomId;
+      setInboxFeedMessages((prev: InboxFeedMessage[]) => [
+        { ...message, content: "", timestamp: "" },
+        ...prev,
+      ]);
+    } else if (!message.content) {
+      setInboxFeedMessages((prev: InboxFeedMessage[]) => [
+        { ...message, content: "", timestamp: "" },
+        ...prev,
+      ]);
+    }
+
     const isAlreadyOpen = conversationBubbles.findIndex(
       (conversation: any) => conversation.roomId === message.roomId
     );
@@ -65,11 +93,21 @@ const MessagingBoxCard: React.FC = () => {
     ]);
   }
 
+  useEffect(() => {
+    async function getLastActiveChats() {
+      const rooms = await apiClient(
+        `chat/user/${user?.id}/rooms/recent?offset=${offset}`
+      );
+      setInboxFeedMessages(rooms);
+    }
+    user && getLastActiveChats();
+  }, [user?.id]);
+
   return (
     <div className={messagingBoxStyle.conversationsQueueContainer}>
       <aside className={messagingBoxStyle.conversationOverlayContainer}>
         <InboxBubble
-          messages={userMessages}
+          messages={inboxFeedMessages}
           addConversationToQueue={addConversationToQueue}
         />
         {conversationBubbles &&
@@ -87,3 +125,11 @@ const MessagingBoxCard: React.FC = () => {
 };
 
 export default MessagingBoxCard;
+
+async function createRoom(userId: string, userId2: string) {
+  const response = await apiClient("chat/single", {
+    data: { users: [userId, userId2] },
+    method: "POST",
+  });
+  return { roomId: response[0].chat_id };
+}
